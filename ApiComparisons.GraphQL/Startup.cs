149a0e72d3333b950ff6f150e7bb9efa4e2f94c5
Jonathan Plata <ajplata0@gmail.com>
2020-02-22 16:16:28 -0600

    Add GraphQL server packages, add stub schema

--- ApiComparisons.GraphQL/Startup.cs
+++ ApiComparisons.GraphQL/Startup.cs
@@ -1,51 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GraphQL.Server;
+using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace ApiComparisons.GraphQL
 {
+    // TODO: Move to shared project
+    public class ChatSchema : Schema
+    {
+    }
+
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddSingleton<ChatSchema>();
+
+            services.AddLogging();            
+
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = Environment.IsDevelopment();
+                options.ExposeExceptions = Environment.IsDevelopment();
+                options.UnhandledExceptionDelegate = context => Console.WriteLine($"Error: {context.OriginalException}");
+            })
+                .AddSystemTextJson(deserializerSettings => { }, serializerSettings => { })
+                .AddGraphTypes(typeof(ChatSchema));
+            //.AddWebSockets() // Add GraphQL.Server.Transports.WebSockets package for websockets support
+            //.AddDataLoader();                            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseWebSockets(); // for websockets support
+            // app.UseGraphQLWebSockets<Schema>("/graphql");
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseGraphQL<ChatSchema>("/graphql");
         }
     }
 }

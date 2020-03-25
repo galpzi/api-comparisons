5fc0c99d2de3f7a8faaddace4afcdd70d95d1c56
Jonathan Plata <ajplata0@gmail.com>
2020-03-25 17:22:49 -0500

    Add remaining proto models, finish implementing TransactionsService queries

--- ApiComparisons.Grpc/Services/TransactionsService.cs
+++ ApiComparisons.Grpc/Services/TransactionsService.cs
@@ -51,5 +51,111 @@ namespace ApiComparisons.Grpc
             response.People.AddRange(repeated);
             return response;
         }
+
+        public override async Task<TransactionResponse> GetTransactions(Empty request, ServerCallContext context)
+        {
+            var response = new TransactionResponse();
+            var transactions = await this.repo.GetTransactionsAsync();
+            var repeated = transactions.Select(o => new Transaction
+            {
+                Id = o.ID.ToString(),
+                PersonId = o.PersonID.ToString(),
+                Total = Convert.ToInt32(o.Total),
+                Created = Timestamp.FromDateTime(o.Created)
+            });
+            response.Transactions.AddRange(repeated);
+            return response;
+        }
+
+        public override async Task<TransactionResponse> GetPersonTransactions(TransactionRequest request, ServerCallContext context)
+        {
+            if (!Guid.TryParse(request.Person.Id, out Guid id))
+                throw new ArgumentException($"invalid person id {request.Person.Id}");
+
+            var response = new TransactionResponse();
+            var person = new Shared.DAL.Person
+            {
+                ID = id,
+                Name = request.Person.Name,
+                Created = request.Person.Created.ToDateTime()
+            };
+            var transactions = await this.repo.GetTransactionsAsync(person);
+            var repeated = transactions.Select(o => new Transaction
+            {
+                Id = o.ID.ToString(),
+                PersonId = o.PersonID.ToString(),
+                Total = Convert.ToInt32(o.Total),
+                Created = Timestamp.FromDateTime(o.Created)
+            });
+            response.Transactions.AddRange(repeated);
+            return response;
+        }
+
+        public override async Task<PurchaseResponse> GetPurchases(PurchaseRequest request, ServerCallContext context)
+        {
+            if (!Guid.TryParse(request.Transaction.Id, out Guid id))
+                throw new ArgumentException($"invalid transaction id {request.Transaction.Id}");
+
+            var response = new PurchaseResponse();
+            var transaction = new Shared.DAL.Transaction { ID = id };
+            var purchases = await this.repo.GetPurchasesAsync(transaction);
+            var repeated = purchases.Select(o => new Purchase
+            {
+                ProductId = o.ProductID.ToString(),
+                TransactionId = o.TransactionID.ToString(),
+                Count = o.Count,
+                Price = Convert.ToInt32(o.Price),
+                Created = Timestamp.FromDateTime(o.Created)
+            });
+            response.Purchases.AddRange(repeated);
+            return response;
+        }
+
+        public override async Task<ProductResponse> GetProduct(ProductRequest request, ServerCallContext context)
+        {
+            if (!Guid.TryParse(request.Purchase.ProductId, out Guid productID))
+                throw new ArgumentException($"invalid purchase product id {request.Purchase.ProductId}");
+
+            if (!Guid.TryParse(request.Purchase.TransactionId, out Guid transactionID))
+                throw new ArgumentException($"invalid purchase transaction id {request.Purchase.TransactionId}");
+
+            var purchase = new Shared.DAL.Purchase { ProductID = productID, TransactionID = transactionID };
+            var product = await this.repo.GetProductAsync(purchase);
+            return new ProductResponse
+            {
+                Product = new Product
+                {
+                    Id = product.ID.ToString(),
+                    StoreId = product.StoreID.ToString(),
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = Convert.ToInt32(product.Price),
+                    Created = Timestamp.FromDateTime(product.Created)
+                }
+            };
+        }
+
+        public override async Task<StoreResponse> GetStore(StoreRequest request, ServerCallContext context)
+        {
+            if (!Guid.TryParse(request.Product.Id, out Guid id))
+                throw new ArgumentException($"invalid product id {request.Product.Id}");
+
+            if (!Guid.TryParse(request.Product.StoreId, out Guid storeID))
+                throw new ArgumentException($"invalid product store id {request.Product.StoreId}");
+
+            var product = new Shared.DAL.Product { ID = id, StoreID = storeID };
+            var store = await this.repo.GetStoreAsync(product);
+            return new StoreResponse
+            {
+                Store = new Store
+                {
+                    Id = store.ID.ToString(),
+                    Name = store.Name,
+                    Country = store.Country,
+                    Address = store.Address,
+                    Created = Timestamp.FromDateTime(store.Created)
+                }
+            };
+        }
     }
 }

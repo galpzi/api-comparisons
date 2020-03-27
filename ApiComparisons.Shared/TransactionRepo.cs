6a39ec04ea06462e018e4275dbf3c455d188212b
Jonathan Plata <ajplata0@gmail.com>
2020-03-27 15:47:52 -0500

    Implement mutations in transaction repo

--- ApiComparisons.Shared/TransactionRepo.cs
+++ ApiComparisons.Shared/TransactionRepo.cs
@@ -19,13 +19,7 @@ namespace ApiComparisons.Shared
             this.context = context;
         }
 
-        public async Task<Person> AddPersonAsync(string name)
-        {
-            var person = await this.context.Persons.AddAsync(new Person { Name = name, Created = DateTime.Now });
-            await this.context.SaveChangesAsync();
-            return person.Entity;
-        }
-
+        #region Queries        
         public async Task<IEnumerable<Person>> GetPeopleAsync()
         {
             return await this.context.Persons.ToListAsync();
@@ -64,5 +58,100 @@ namespace ApiComparisons.Shared
                 .Where(o => o.TransactionID == transaction.ID)
                 .ToListAsync();
         }
+        #endregion
+
+        #region Mutations
+        public async Task<Store> AddStoreAsync(Store store)
+        {
+            this.context.Entry(store).State = store.ID == Guid.Empty ? EntityState.Added : EntityState.Modified;
+            await this.context.SaveChangesAsync();
+            return store;
+        }
+
+        public async Task<Store> RemoveStoreAsync(Guid id)
+        {
+            var store = await this.context.Stores.FindAsync(id);
+            if (store != null)
+            {
+                this.context.Remove(store);
+                await this.context.SaveChangesAsync();
+            }
+            return store;
+        }
+
+        public async Task<Person> AddPersonAsync(string name)
+        {
+            var person = await this.context.Persons.AddAsync(new Person { Name = name, Created = DateTime.Now });
+            await this.context.SaveChangesAsync();
+            return person.Entity;
+        }
+
+        public async Task<Person> RemovePersonAsync(Guid id)
+        {
+            var person = await this.context.Persons.FindAsync(id);
+            if (person != null)
+            {
+                this.context.Persons.Remove(person);
+                await this.context.SaveChangesAsync();
+            }
+            return person;
+        }
+
+        public async Task<Product> AddProductAsync(Product product)
+        {
+            this.context.Entry(product).State = product.ID == Guid.Empty ? EntityState.Added : EntityState.Modified;
+            await this.context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> RemoveProductAsync(Guid id)
+        {
+            var product = await this.context.Products.FindAsync(id);
+            if (product != null)
+            {
+                this.context.Remove(product);
+                await this.context.SaveChangesAsync();
+            }
+            return product;
+        }
+
+        public async Task<Purchase> AddPurchaseAsync(Purchase purchase)
+        {
+            var added = purchase.ProductID == Guid.Empty && purchase.TransactionID == Guid.Empty;
+            this.context.Entry(purchase).State = added ? EntityState.Added : EntityState.Modified;
+            await this.context.SaveChangesAsync();
+            return purchase;
+        }
+
+        public async Task<Purchase> RemovePurchaseAsync(Guid purchaseID, Guid transactionID)
+        {
+            var purchase = await this.context.Purchases.FindAsync(purchaseID, transactionID);
+            if (purchase != null)
+            {
+                this.context.Purchases.Remove(purchase);
+                await this.context.SaveChangesAsync();
+            }
+            return purchase;
+        }
+
+        public async Task<Transaction> AddTransactionAsync(Transaction transaction)
+        {
+            var added = transaction.ID == Guid.Empty && transaction.PersonID == Guid.Empty;
+            this.context.Entry(transaction).State = added ? EntityState.Added : EntityState.Modified;
+            await this.context.SaveChangesAsync();
+            return transaction;
+        }
+
+        public async Task<Transaction> RemoveTransactionAsync(Guid id, Guid personID)
+        {
+            var transaction = await this.context.Transactions.FindAsync(id, personID);
+            if (transaction != null)
+            {
+                this.context.Transactions.Remove(transaction);
+                await this.context.SaveChangesAsync();
+            }
+            return transaction;
+        }
+        #endregion
     }
 }

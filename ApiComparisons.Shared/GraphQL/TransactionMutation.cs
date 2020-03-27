e78e9fb19f327cd4a36d3f009c432f7c444c26eb
Jonathan Plata <ajplata0@gmail.com>
2020-03-27 16:42:12 -0500

    Implement GraphQL transaction mutations

--- ApiComparisons.Shared/GraphQL/TransactionMutation.cs
+++ ApiComparisons.Shared/GraphQL/TransactionMutation.cs
@@ -1,4 +1,8 @@
-﻿using GraphQL.Types;
+﻿using ApiComparisons.Shared.DAL;
+using ApiComparisons.Shared.GraphQL.Types;
+using GraphQL;
+using GraphQL.Types;
+using System;
 
 namespace ApiComparisons.Shared.GraphQL
 {
@@ -7,6 +11,102 @@ namespace ApiComparisons.Shared.GraphQL
         public TransactionMutation(ITransactionRepo repo)
         {
             Name = "TransactionsMutation";
+            Field<PersonType>(
+                name: "addPerson",
+                description: "Adds a person",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "name" }),
+                resolve: context =>
+                {
+                    var name = context.GetArgument<string>("name");
+                    return repo.AddPersonAsync(name);
+                });
+            Field<PersonType>(
+                name: "removePerson",
+                description: "Removes a person",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "id" }),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<Guid>("id");
+                    return repo.RemovePersonAsync(id);
+                });
+            Field<StoreType>(
+                name: "addStore",
+                description: "Adds a store",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StoreType>> { Name = "store" }),
+                resolve: context =>
+                {
+                    var store = context.GetArgument<Store>("store");
+                    return repo.AddStoreAsync(store);
+                });
+            Field<StoreType>(
+                name: "removeStore",
+                description: "Removes a store",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "id" }),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<Guid>("id");
+                    return repo.RemoveStoreAsync(id);
+                });
+            Field<ProductType>(
+                name: "addProduct",
+                description: "Adds a product",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<ProductType>> { Name = "product" }),
+                resolve: context =>
+                {
+                    var product = context.GetArgument<Product>("product");
+                    return repo.AddProductAsync(product);
+                });
+            Field<StoreType>(
+                name: "removeProduct",
+                description: "Removes a product",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "id" }),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<Guid>("id");
+                    return repo.RemoveProductAsync(id);
+                });
+            Field<PurchaseType>(
+                name: "addPurchase",
+                description: "Adds a purchase",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<PurchaseType>> { Name = "purchase" }),
+                resolve: context =>
+                {
+                    var purchase = context.GetArgument<Purchase>("purchase");
+                    return repo.AddPurchaseAsync(purchase);
+                });
+            Field<PurchaseType>(
+                name: "removePurchase",
+                description: "Removes a purchase",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "productID" },
+                    new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "transactionID" }),
+                resolve: context =>
+                {
+                    var productID = context.GetArgument<Guid>("productID");
+                    var transactionID = context.GetArgument<Guid>("transactionID");
+                    return repo.RemovePurchaseAsync(productID, transactionID);
+                });
+            Field<TransactionType>(
+                name: "addTransaction",
+                description: "Adds a transaction",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<TransactionType>> { Name = "transaction" }),
+                resolve: context =>
+                {
+                    var transaction = context.GetArgument<Transaction>("transaction");
+                    return repo.AddTransactionAsync(transaction);
+                });
+            Field<PurchaseType>(
+                name: "removeTransaction",
+                description: "Removes a transaction",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "id" },
+                    new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "personID" }),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<Guid>("id");
+                    var personID = context.GetArgument<Guid>("personID");
+                    return repo.RemoveTransactionAsync(id, personID);
+                });
         }
     }
 }

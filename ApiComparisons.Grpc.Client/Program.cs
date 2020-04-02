d9f696ff6b23194851419ba80b012952abf7952a
Jonathan Plata <ajplata0@gmail.com>
2020-04-02 00:47:33 -0500

    Simplify transaction commands, remove hosted service

--- ApiComparisons.Grpc.Client/Program.cs
+++ ApiComparisons.Grpc.Client/Program.cs
@@ -1,10 +1,4 @@
-﻿using ApiComparisons.Shared.GRPC;
-using ApiComparisons.Shared.GRPC.Models;
-using ApiComparisons.Shared.Protos;
-using ApiComparisons.Shared.StarWars.GRPC;
-using ApiComparisons.Shared.StarWars.GRPC.Characters;
-using Grpc.Net.Client;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,22 +6,18 @@ using System;
 using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
-using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ApiComparisons.Grpc.Client
 {
-    class Program
+    public class Program
     {
         static Task Main(string[] args)
         {
-            var root = TransactionCommands.Root();
-            root.Handler = CommandHandler.Create<IHost>(Run);
-            var parser = new CommandLineBuilder(root)
+            var parser = new CommandLineBuilder(TransactionCommands.Root())
                 .UseDefaults()
-                .UseHost(builder => CreateHostBuilder(args))
+                .UseHost(CreateHostBuilder)
                 .Build();
             return parser.InvokeAsync(args);
         }
@@ -47,63 +37,12 @@ namespace ApiComparisons.Grpc.Client
                 {
                     services.AddLogging();
                     services.Configure<AppSettings>(context.Configuration.GetSection("Settings"));
-                    services.AddHostedService<TransactionCommandService>();
                 })
                 .ConfigureLogging((context, builder) =>
                 {
                     builder.AddConsole();
                     builder.AddConfiguration(context.Configuration);
                 });
-        }
-
-        static async Task Greet()
-        {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(new HelloRequest
-            {
-                Name = "Client"
-            });
-
-            Console.WriteLine($"Greeting: {reply.Message}");
-            Console.WriteLine($"Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        static async Task StarWars()
-        {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new StarWars.StarWarsClient(channel);
-            var response = await client.GetCharacterAsync(new CharacterRequest
-            {
-                Name = "Boba Fett"
-            });
-
-            Console.WriteLine($"Character ID: {response.Character.Id}, Name: {response.Character.Name}");
-            Console.WriteLine($"Character Friends: {response.Character.Friends.Count}, Appears In: {response.Character.AppearsIn.Count}");
-            Console.WriteLine($"Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        static async Task Transactions()
-        {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Transactions.TransactionsClient(channel);
-            var response = await client.GetPersonAsync(new PersonRequest
-            {
-                Id = ""
-            });
-
-            Console.WriteLine($"Character ID: {response.Person.Id}, Name: {response.Person.Name}, Created: {response.Person.Created}");
-            Console.WriteLine($"Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        internal static Task Run(IHost host)
-        {
-            var services = host.Services;
-            var lifetime = services.GetRequiredService<IHostApplicationLifetime>();
-            return Task.Delay(Timeout.InfiniteTimeSpan, lifetime.ApplicationStopped);
         }
     }
 }
--- ApiComparisons.Grpc.Client/TransactionCommands.cs
+++ ApiComparisons.Grpc.Client/TransactionCommands.cs
@@ -1,6 +1,17 @@
 ﻿using ApiComparisons.Shared.DAL;
+using ApiComparisons.Shared.GRPC;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ApiComparisons.Grpc.Client
 {
@@ -8,58 +19,176 @@ namespace ApiComparisons.Grpc.Client
     {
         public static RootCommand Root()
         {
-            return new RootCommand("CLI client for the ApiComparisons GRPC server")
+            var root = new RootCommand("CLI client for the ApiComparisons GRPC server")
             {
                 new Command("transactions", "transaction GRPC service commands")
                 {
-                    GetQueryCommands(),
-                    GetMutationCommands()
+                    GetPeopleCommand(),
+                    GetStoreCommand(),
+                    //new Command("products", "Return, add or remove products.")
+                    //{
+                    //    new Option<Purchase>("--purchase", "The parent purchase") { Required = true },
+                    //    new Command("add", "Add a product.") { new Option<Product>("--product", "The product to add.") { Required = true } },
+                    //    new Command("remove", "Remove a product.") { new Option<Product>("--product", "The product to remove.") { Required = true } }
+                    //},
+                    //new Command("purchases", "Return, add or remove purchases")
+                    //{
+                    //    new Option<Transaction>("--transaction", "The parent transaction") { Required = true },
+                    //    new Command("add", "Add a purchase.") { new Option<Purchase>("--purchase", "The purchase to add.") { Required = true }},
+                    //    new Command("remove", "Remove a purchase.") { new Option<Purchase>("--purchase", "The purchase to remove.") { Required = true }}
+                    //},
+                    //new Command("transactions", "Return, add or remove transactions")
+                    //{
+                    //    new Option<Person>("--person", "The transaction owner. The default is null to indicate no person specified."),
+                    //    new Command("add", "Add a transaction.") { new Option<Transaction>("--transaction", "The transaction to add.") { Required = true } },
+                    //    new Command("remove", "Remove a transaction.") { new Option<Transaction>("--transaction", "The transaction to remove.") { Required = true } },
+                    //}
                 }
             };
+            root.Handler = CommandHandler.Create<IHost>(Run);
+            return root;
         }
 
-        private static Command GetQueryCommands()
+        internal static void Handle(dynamic response) =>
+            Console.WriteLine((string)JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true }));
+
+        internal static Task Run(IHost host)
         {
-            return new Command("query", "GRPC service queries.")
+            var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+            return Task.Delay(Timeout.InfiniteTimeSpan, lifetime.ApplicationStopped);
+        }
+
+        #region People
+        private static Command GetPeopleCommand()
+        {
+            var people = new Command("people", "Return, add or remove people.");
+            people.Add(new Option<Guid>("--id", "The persons's ID. The default is empty to indicate no ID specified."));
+            people.Handler = CommandHandler.Create(typeof(TransactionCommands).GetMethod(nameof(GetPeople), BindingFlags.Static | BindingFlags.NonPublic));
+
+            var add = new Command("add", "Add a person.") { new Option<Person>("--person", "The person to add.") { Required = true } };
+            add.Handler = CommandHandler.Create(typeof(TransactionCommands).GetMethod(nameof(AddPerson), BindingFlags.Static | BindingFlags.NonPublic));
+
+            var remove = new Command("remove", "Remove a person.") { new Option<Person>("--person", "The person to remove.") { Required = true } };
+            remove.Handler = CommandHandler.Create(typeof(TransactionCommands).GetMethod(nameof(RemovePerson), BindingFlags.Static | BindingFlags.NonPublic));
+
+            people.Add(add);
+            people.Add(remove);
+            return people;
+        }
+
+        private static async Task GetPeople(IHost host, Guid id)
+        {
+            dynamic response;
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+            using var channel = GrpcChannel.ForAddress(options.Value.ServerUri);
+            var client = new Transactions.TransactionsClient(channel);
+            if (id == Guid.Empty)
+                response = await client.GetPeopleAsync(new Google.Protobuf.WellKnownTypes.Empty());
+            else
+                response = await client.GetPersonAsync(new Shared.GRPC.Models.PersonRequest { Id = id.ToString() });
+            Handle(response);
+        }
+
+        private static async Task AddPerson(IHost host, Person person)
+        {
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+            using var channel = GrpcChannel.ForAddress(options.Value.ServerUri);
+            var client = new Transactions.TransactionsClient(channel);
+            var response = await client.AddPersonAsync(new Shared.GRPC.Models.PersonRequest { Id = person.ID.ToString(), Name = person.Name });
+            Handle(response);
+        }
+
+        private static async Task RemovePerson(IHost host, Person person)
+        {
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+            using var channel = GrpcChannel.ForAddress(options.Value.ServerUri);
+            var client = new Transactions.TransactionsClient(channel);
+            var response = await client.RemovePersonAsync(new Shared.GRPC.Models.PersonRequest { Id = person.ID.ToString(), Name = person.Name });
+            Handle(response);
+        }
+        #endregion
+
+        #region Stores 
+        private static Command GetStoreCommand()
+        {
+            var stores = new Command("stores", "Return, add or remove stores.")
             {
-                new Command("people", "Returns people.") { new Option<Guid>("--id", "The persons's ID. The default is empty to indicate no ID specified.") },
-                new Command("stores", "Returns stores.") { new Option<Product>("--product", "The product sold at this store") { Required = true } },
-                new Command("products", "Returns products.") { new Option<Purchase>("--purchase", "The parent purchase") { Required = true } },
-                new Command("purchases", "Returns purchases.") { new Option<Transaction>("--transaction", "The parent transaction") { Required = true } },
-                new Command("transactions", "Returns transactions.") { new Option<Person>("--person", "The transaction owner. The default is null to indicate no person specified.") },
+                new Option<Product>("--product", "The product sold at this store") { Required = true }
             };
+            stores.Handler = CommandHandler.Create(typeof(TransactionCommands).GetMethod(nameof(GetStores), BindingFlags.Static | BindingFlags.NonPublic));
+
+            var add = new Command("add", "Add a store.") { new Option<Store>("--store", "The store to add.") { Required = true } };
+            add.Handler = CommandHandler.Create(typeof(TransactionCommands).GetMethod(nameof(AddStore), BindingFlags.Static | BindingFlags.NonPublic));
+
+            var remove = new Command("remove", "Remove a store.") { new Option<Store>("--store", "The store to remove.") { Required = true } };
+            remove.Handler = CommandHandler.Create(typeof(TransactionCommands).GetMethod(nameof(RemoveStore), BindingFlags.Static | BindingFlags.NonPublic));
+            stores.Add(add);
+            stores.Add(remove);
+            return stores;
         }
 
-        private static Command GetMutationCommands()
+        private static async Task GetStores(IHost host, Product product)
         {
-            return new Command("mutation", "GRPC service mutations.")
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+            using var channel = GrpcChannel.ForAddress(options.Value.ServerUri);
+            var client = new Transactions.TransactionsClient(channel);
+            var response = await client.GetStoreAsync(new Shared.GRPC.Models.StoreRequest
             {
-                new Command("people", "Add or remove people.")
+                Product = new Shared.GRPC.Models.Product
                 {
-                    new Command("add", "Add a person.") { new Option<Person>("--person", "The person to add.") { Required = true } },
-                    new Command("remove", "Remove a person.") { new Option<Person>("--person", "The person to remove.") { Required = true } }
-                },
-                new Command("stores", "Add or remove stores.")
-                {
-                    new Command("add", "Add a store.") { new Option<Store>("--store", "The store to add.") { Required = true } },
-                    new Command("remove", "Remove a store.") { new Option<Store>("--store", "The store to remove.") { Required = true } }
-                },
-                new Command("products", "Add or remove products.")
-                {
-                    new Command("add", "Add a product.") { new Option<Product>("--product", "The product to add.") { Required = true } },
-                    new Command("remove", "Remove a product.") { new Option<Product>("--product", "The product to remove.") { Required = true } }
-                },
-                new Command("purchases", "Add or remove purchases")
-                {
-                    new Command("add", "Add a purchase.") { new Option<Purchase>("--purchase", "The purchase to add.") { Required = true }},
-                    new Command("remove", "Remove a purchase.") { new Option<Purchase>("--purchase", "The purchase to remove.") { Required = true }}
-                },
-                new Command("transactions", "Add or remove transactions")
-                {
-                    new Command("add", "Add a transaction.") { new Option<Transaction>("--transaction", "The transaction to add.") { Required = true } },
-                    new Command("remove", "Remove a transaction.") { new Option<Transaction>("--transaction", "The transaction to remove.") { Required = true } },
+                    Id = product.ID.ToString(),
+                    StoreId = product.StoreID.ToString()
                 }
-            };
+            });
+            Handle(response);
         }
+
+        private static async Task AddStore(IHost host, Store store)
+        {
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+            using var channel = GrpcChannel.ForAddress(options.Value.ServerUri);
+            var client = new Transactions.TransactionsClient(channel);
+            var response = await client.AddStoreAsync(new Shared.GRPC.Models.StoreRequest
+            {
+                Store = new Shared.GRPC.Models.Store
+                {
+                    Name = store.Name,
+                    Id = store.ID.ToString(),
+                    Address = store.Address,
+                    Country = store.Country,
+                    Created = Timestamp.FromDateTime(store.Created)
+                }
+            });
+            Handle(response);
+        }
+
+        private static async Task RemoveStore(IHost host, Store store)
+        {
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+            using var channel = GrpcChannel.ForAddress(options.Value.ServerUri);
+            var client = new Transactions.TransactionsClient(channel);
+            var response = await client.RemoveStoreAsync(new Shared.GRPC.Models.StoreRequest
+            {
+                Store = new Shared.GRPC.Models.Store
+                {
+                    Name = store.Name,
+                    Id = store.ID.ToString(),
+                    Address = store.Address,
+                    Country = store.Country,
+                    Created = Timestamp.FromDateTime(store.Created)
+                }
+            });
+            Handle(response);
+        }
+        #endregion
+
+        #region Products
+        #endregion
+
+        #region Purchases
+        #endregion
+
+        #region Transactions
+        #endregion
     }
 }

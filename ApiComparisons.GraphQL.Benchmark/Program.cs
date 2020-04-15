ffc6d0718ee8deb60308264ca350eb11f510305e
Jonathan Plata <ajplata0@gmail.com>
2020-04-15 17:27:11 -0500

    [WIP] Add GraphQL benchmark project

---
+++ ApiComparisons.GraphQL.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiComparisons.GraphQL.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

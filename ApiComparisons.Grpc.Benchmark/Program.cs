20cfa736e53a8bd9922b388028da7f3b71abd476
Jonathan Plata <ajplata0@gmail.com>
2020-04-15 22:58:16 -0500

    Add gRPC benchmark project

---
+++ ApiComparisons.Grpc.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiComparisons.Grpc.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
